6a01e853f22435cdb7fe95dc5b84e42af48e7db6
Andras Nemeth <nemethand94@gmail.com>
2022-10-25 13:33:13 +0200

    appservice credentials set

--- src/services/appservice.service.ts
+++ src/services/appservice.service.ts
@@ -33,26 +33,26 @@ export class AppService {
     async init(envStyle: EnvStyle): Promise<CustomError | boolean> {
         console.log(`${this.name} init start... nevironment style: ${EnvStyle[envStyle]}`);
         try {
-            // const credentials = {
-            //     type: process.env.SA_TYPE,
-            //     project_id: process.env.SA_PROJECT_ID,
-            //     private_key_id: process.env.SA_PRIVATE_KEY_ID,
-            //     private_key: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
-            //     client_email: process.env.CLIENT_EMAIL,
-            //     client_id: process.env.CLIENT_ID,
-            //     auth_uri: process.env.AUTH_URI,
-            //     token_uri: process.env.TOKEN_URI,
-            //     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
-            //     client_x509_cert_url: process.env.AUTH_CLIENT_CERT_URL
-            // };
-            const cred: ServiceAccount = {
-                projectId: process.env.SA_PROJECT_ID,
-                clientEmail: process.env.CLIENT_EMAIL,
-                privateKey: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
-            }
+            const credentials = {
+                type: process.env.SA_TYPE,
+                project_id: process.env.SA_PROJECT_ID,
+                private_key_id: process.env.SA_PRIVATE_KEY_ID,
+                private_key: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                client_email: process.env.CLIENT_EMAIL,
+                client_id: process.env.CLIENT_ID,
+                auth_uri: process.env.AUTH_URI,
+                token_uri: process.env.TOKEN_URI,
+                auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+                client_x509_cert_url: process.env.AUTH_CLIENT_CERT_URL
+            };
+            // const cred: ServiceAccount = {
+            //     projectId: process.env.SA_PROJECT_ID,
+            //     clientEmail: process.env.CLIENT_EMAIL,
+            //     privateKey: process.env.SA_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            // }
 
             this.setApp(await initializeApp({
-                credential: cert(cred),
+                credential: cert(credentials as ServiceAccount),
                 storageBucket: process.env.CLOUD_PROJECT_ID },
                 'todo-back'
             ));

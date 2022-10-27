1fb310458cfb4fcdf0a4e6bfc1026e099c9a133f
Andras Nemeth <nemethand94@gmail.com>
2022-10-27 22:39:57 +0200

    add cors to authRouter

--- src/api/auth.router.ts
+++ src/api/auth.router.ts
@@ -10,16 +10,19 @@ import { AuthService } from '../services/auth.service';
 import { validateJSONSchema } from '../handlers/validators/schema.validator';
 import { userAuthHandler } from '../handlers/operations/auth/user-auth-handler';
 import { tokenRefreshHandler } from '../handlers/operations/auth/token-refresh.handler';
+import cors from 'cors';
 
 
 function authRouter(app: Express, authService: AuthService) {
 
-    app.post('/api/login', 
+    app.post('/api/login',
+    cors(),
     validateJSONSchema(exportSchemas.requestSchema, true),
     validateJSONSchema(exportSchemas.loginSchema, false),
     userAuthHandler(authService)),
 
     app.post('/api/refresh',
+    cors(),
     validateJSONSchema(exportSchemas.tokensSchema, false),
     tokenRefreshHandler(authService))
 }
